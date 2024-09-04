87864ba8dfa78140adaf72d0dfa5824ad0bbbaad
UshurFatima <ushurfatima@gmail.com>
2024-09-04 17:33:24 +0600

    First Commit

--- blog/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render  # для преобразования моделей, html-шаблонов, логики
-from django.http import HttpResponse
-
-
-def hello_view(request):
-    return HttpResponse('hello world!!!')
