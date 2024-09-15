7e9697c05b84f26d1021aa2e198e1931a9e78d84
UshurFatima <ushurfatima@gmail.com>
2024-09-15 13:15:53 +0600

    CRUD

--- books/urls.py
+++ books/urls.py
@@ -5,6 +5,12 @@ urlpatterns = [
     path('book_list/', views.book_list_view),
     path('book_list/<int:id>/', views.book_detail_view),
 
+    path('add_book/', views.add_book_view, name='add_book'),
+    path('book_list_delete/', views.book_list_delete_view, name='book_list_delete'),
+    path('book_list/<int:id>/delete/', views.book_drop_view),
+    path('book_list_edit/', views.book_list_edit_view, name='book_list_edit'),
+    path('book_list/<int:id>/edit/', views.update_book_view),
+
     path('myinfo/', views.myinfo_view),
     path('friend/', views.friend_view),
     path('time/', views.time_view),
--- books/views.py
+++ books/views.py
@@ -1,7 +1,75 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from datetime import datetime
-from . import models
+from . import models, forms
+
+
+def add_book_view(request):
+    if request.method == 'POST':
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Книга была успешно добавлена! <a href = "/book_list/">На список книг</a>')
+    else:
+        form = forms.BookForm()
+    return render(
+        request,
+        template_name='crud/add_book.html',
+        context={
+            'form': form
+        }
+    )
+
+
+def book_list_delete_view(request):
+    if request.method == "GET":
+        book_object = models.Book.objects.all()
+        return render(
+            request,
+            template_name='crud/book_list_delete.html',
+            context={
+                'book_object': book_object
+            }
+        )
+
+
+def book_drop_view(request, id):
+    book_id = get_object_or_404(models.Book, id=id)
+    book_id.delete()
+    return HttpResponse('Книга была успешно удалена! <a href = "/book_list/">На список книг</a>')
+
+
+def book_list_edit_view(request):
+    if request.method == "GET":
+        book_object = models.Book.objects.all()
+        return render(
+            request,
+            template_name='crud/book_list_edit.html',
+            context={
+                'book_object': book_object
+            }
+        )
+
+
+def update_book_view(request, id):
+    book_id = get_object_or_404(models.Book, id=id)
+    if request.method == "POST":
+        form = forms.BookForm(request.POST, instance=book_id)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Информация о книге была успешно отредактирована! '
+                                '<a href = "/book_list/">На список книг</a>')
+    else:
+        form = forms.BookForm(instance=book_id)
+
+    return render(
+        request,
+        template_name='crud/update_book.html',
+        context={
+            'form': form,
+            'book_id': book_id
+        }
+    )
 
 
 def book_list_view(request):
@@ -18,7 +86,7 @@ def book_list_view(request):
 
 def book_detail_view(request, id):
     if request.method == "GET":
-        book_id = get_object_or_404(models.Book, id=id)
+        book_id = get_object_or_404(models. Book, id=id)
         return render(
             request,
             template_name='book_detail.html',
