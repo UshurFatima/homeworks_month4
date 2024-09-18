8eb786dccc341531892045fd688915166971a53c
UshurFatima <ushurfatima@gmail.com>
2024-09-18 19:06:00 +0600

    CBV and Search

--- books/urls.py
+++ books/urls.py
@@ -2,14 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('book_list/', views.book_list_view),
-    path('book_list/<int:id>/', views.book_detail_view),
+    path('book_list/', views.BookListView.as_view(), name='book_list'),
+    path('book_list/<int:id>/', views.BookDetailView.as_view()),
 
-    path('add_book/', views.add_book_view, name='add_book'),
-    path('book_list_delete/', views.book_list_delete_view, name='book_list_delete'),
-    path('book_list/<int:id>/delete/', views.book_drop_view),
-    path('book_list_edit/', views.book_list_edit_view, name='book_list_edit'),
-    path('book_list/<int:id>/edit/', views.update_book_view),
+    path('add_book/', views.AddBookView.as_view(), name='add_book'),
+    path('book_list_delete/', views.BookListDeleteView.as_view(), name='book_list_delete'),
+    path('book_list/<int:id>/delete/', views.BookDropView.as_view()),
+    path('book_list_edit/', views.BookListEditView.as_view(), name='book_list_edit'),
+    path('book_list/<int:id>/edit/', views.UpdateBookView.as_view()),
+
+    path('search/', views.SearchView.as_view(), name='search'),
 
     path('myinfo/', views.myinfo_view),
     path('friend/', views.friend_view),
--- books/views.py
+++ books/views.py
@@ -1,99 +1,181 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views import generic
 from datetime import datetime
 from . import models, forms
 
 
-def add_book_view(request):
-    if request.method == 'POST':
-        form = forms.BookForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Книга была успешно добавлена! <a href = "/book_list/">На список книг</a>')
-    else:
-        form = forms.BookForm()
-    return render(
-        request,
-        template_name='crud/add_book.html',
-        context={
-            'form': form
-        }
-    )
+class SearchView(generic.ListView):
+    template_name = 'book_list.html'
+    context_object_name = 'book_object'
+    paginate_by = 5  # делит объекты по страницам
+
+    def get_queryset(self):  # чтобы получать книги по q (то, что мы вводим в поиск)
+        return models.Book.objects.filter(title__icontains=self.request.GET.get('q')).order_by('-id')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        # чтобы данные выводились по запросу (получаем их)
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')  # соединяем контекст с вьюшкой
+        return context
 
 
-def book_list_delete_view(request):
-    if request.method == "GET":
-        book_object = models.Book.objects.all()
-        return render(
-            request,
-            template_name='crud/book_list_delete.html',
-            context={
-                'book_object': book_object
-            }
-        )
+class AddBookView(generic.CreateView):
+    template_name = 'crud/add_book.html'
+    form_class = forms.BookForm
+    success_url = '/book_list/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)  # сохраняем информацию из формы
+        # чтобы сохраненная информация отправилась в базу данных
+        return super(AddBookView, self).form_valid(form=form)
 
 
-def book_drop_view(request, id):
-    book_id = get_object_or_404(models.Book, id=id)
-    book_id.delete()
-    return HttpResponse('Книга была успешно удалена! <a href = "/book_list/">На список книг</a>')
+# def add_book_view(request):
+#     if request.method == 'POST':
+#         form = forms.BookForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Книга была успешно добавлена! <a href = "/book_list/">На список книг</a>')
+#     else:
+#         form = forms.BookForm()
+#     return render(
+#         request,
+#         template_name='crud/add_book.html',
+#         context={
+#             'form': form
+#         }
+#     )
 
 
-def book_list_edit_view(request):
-    if request.method == "GET":
-        book_object = models.Book.objects.all()
-        return render(
-            request,
-            template_name='crud/book_list_edit.html',
-            context={
-                'book_object': book_object
-            }
-        )
+class BookListDeleteView(generic.ListView):
+    template_name = 'crud/book_list_delete.html'
+    context_object_name = 'book_object'
+    model = models.Book
+
+    def get_queryset(self):
+        return self.model.objects.all().order_by('-id')
 
 
-def update_book_view(request, id):
-    book_id = get_object_or_404(models.Book, id=id)
-    if request.method == "POST":
-        form = forms.BookForm(request.POST, instance=book_id)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Информация о книге была успешно отредактирована! '
-                                '<a href = "/book_list/">На список книг</a>')
-    else:
-        form = forms.BookForm(instance=book_id)
+class BookDropView(generic.DeleteView):
+    template_name = 'crud/confirm_delete.html'
+    success_url = '/book_list_delete/'
 
-    return render(
-        request,
-        template_name='crud/update_book.html',
-        context={
-            'form': form,
-            'book_id': book_id
-        }
-    )
+    def get_object(self, **kwargs):
+        book_id = self.kwargs.get('id')
+        return get_object_or_404(models.Book, id=book_id)
 
 
-def book_list_view(request):
-    if request.method == "GET":
-        book_object = models.Book.objects.all()
-        return render(
-            request,
-            template_name='book_list.html',
-            context={
-                'book_object': book_object
-            }
-        )
+# def book_list_delete_view(request):
+#     if request.method == "GET":
+#         book_object = models.Book.objects.all()
+#         return render(
+#             request,
+#             template_name='crud/book_list_delete.html',
+#             context={
+#                 'book_object': book_object
+#             }
+#         )
 
 
-def book_detail_view(request, id):
-    if request.method == "GET":
-        book_id = get_object_or_404(models. Book, id=id)
-        return render(
-            request,
-            template_name='book_detail.html',
-            context={
-                'book_id': book_id
-            }
-        )
+# def book_drop_view(request, id):
+#     book_id = get_object_or_404(models.Book, id=id)
+#     book_id.delete()
+#     return HttpResponse('Книга была успешно удалена! <a href = "/book_list/">На список книг</a>')
+
+
+class BookListEditView(generic.ListView):
+    template_name = 'crud/book_list_edit.html'
+    context_object_name = 'book_object'
+    model = models.Book
+
+    def get_queryset(self):
+        return self.model.objects.all().order_by('-id')
+
+
+class UpdateBookView(generic.UpdateView):
+    template_name = 'crud/update_book.html'
+    form_class = forms.BookForm
+    success_url = '/book_list_edit/'
+
+    def get_object(self, **kwargs):
+        book_id = self.kwargs.get('id')
+        return get_object_or_404(models.Book, id=book_id)
+
+
+# def book_list_edit_view(request):
+#     if request.method == "GET":
+#         book_object = models.Book.objects.all()
+#         return render(
+#             request,
+#             template_name='crud/book_list_edit.html',
+#             context={
+#                 'book_object': book_object
+#             }
+#         )
+
+
+# def update_book_view(request, id):
+#     book_id = get_object_or_404(models.Book, id=id)
+#     if request.method == "POST":
+#         form = forms.BookForm(request.POST, instance=book_id)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Информация о книге была успешно отредактирована! '
+#                                 '<a href = "/book_list/">На список книг</a>')
+#     else:
+#         form = forms.BookForm(instance=book_id)
+#
+#     return render(
+#         request,
+#         template_name='crud/update_book.html',
+#         context={
+#             'form': form,
+#             'book_id': book_id
+#         }
+#     )
+
+
+class BookListView(generic.ListView):
+    template_name = 'book_list.html'
+    context_object_name = 'book_object'
+    model = models.Book
+
+    def get_queryset(self):  # метод, отвечающий за вывод данных, только общий список
+        return self.model.objects.all().order_by('-id')
+
+
+# def book_list_view(request):
+#     if request.method == "GET":
+#         book_object = models.Book.objects.all()
+#         return render(
+#             request,
+#             template_name='book_list.html',
+#             context={
+#                 'book_object': book_object
+#             }
+#         )
+
+
+class BookDetailView(generic.DetailView):
+    template_name = 'book_detail.html'
+    context_object_name = 'book_id'
+
+    def get_object(self, **kwargs):
+        book_id = self.kwargs.get('id')  # чтобы получить id из параметра kwargs
+        return get_object_or_404(models.Book, id=book_id)
+
+
+# def book_detail_view(request, id):
+#     if request.method == "GET":
+#         book_id = get_object_or_404(models. Book, id=id)
+#         return render(
+#             request,
+#             template_name='book_detail.html',
+#             context={
+#                 'book_id': book_id
+#             }
+#         )
 
 
 def myinfo_view(request):
